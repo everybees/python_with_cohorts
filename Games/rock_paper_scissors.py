d659c03c60810ab12ef5306d8728f6d3c7078f9c
Oludare David <oluwaremioludare@gmail.com>
2022-01-12 00:45:52 +0100

    added Games folder

---
+++ Games/rock_paper_scissors.py
@@ -0,0 +1,62 @@
+import math
+
+# radius = input("Enter a number")
+# radius_convert = int(radius)
+
+# circum = 2 * math.pi * radius_convert
+# print(circum)
+
+# area = math.pi * radius_convert ** 2
+# print(area)
+
+
+#Rock Paper Scissors
+# rock = "rock"
+# paper = "paper"
+# scissors = "scissors"
+
+
+
+# player1_score = 0
+# player2_score = 0
+
+# for i in range(3):
+#     user1 = input("Player1: Enter rock, paper or scissors: ")
+#     user2 = input("Player2: Enter rock, paper, or scissors: ")
+
+#     if user1 == rock and user2 == paper:
+#         print("Result = Player2: paper wins")
+#         player2_score += 1
+#     elif user1 == paper and user2 == rock:
+#         print("Result = Player1: paper wins")
+#         player1_score += 1
+        
+#     elif user1 == rock and user2 == scissors:
+#         print("Result = Player1: rock wins")
+#         player1_score += 1
+#     elif user1 == scissors and user2 == rock:
+#         print("Result = Player2: rock wins")
+#         player2_score += 1
+
+#     elif user1 == scissors and user2 == paper:
+#         print("Result = Player1: scissors wins")
+#         player1_score += 1
+#     elif user1 == paper and user2 == scissors:
+#         print("Result = Player2: scissors wins")
+#         player2_score += 1
+#     break
+
+# print(player1_score)
+# print(player2_score)
+    # if user1 == user2:
+        # print("Draw")
+        
+    
+
+
+
+
+
+#Ten roounds, two players, output their score.
+
+#create folders for each games and push on github/create a pull request
