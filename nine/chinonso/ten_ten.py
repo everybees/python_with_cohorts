06e3d345f3ad3419acfa40a7b38c4e195dcfae4e
chinonso Nwokonko <nwokonkochinonso@gmail.com>
2022-01-11 19:01:06 +0100

    assignment

--- nine/chinonso/ten_ten.py
+++ nine/chinonso/ten_ten.py
@@ -1,5 +1,11 @@
 count1 = 0
 count2 = 0
-for k in range(10):
-    player1 = input('Enter your choice, right and left :')
-    if
+for k in range(3):
+    player1 = input('Player 1, Enter your choice, right and left :')
+    player2 = input('Player 2, Enter your choice, right and left :')
+    if player1 == player2 :
+        count2 +=1
+    if player1 != player2:
+        count1 += 1
+print("Player 1 has ",count1,"point")
+print("Player 2 has ",count2,"point")
