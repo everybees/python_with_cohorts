cd459f257f47d408471b85c3cbb3050cf22e5e7b
Ibrahim Ajao <ajaoibra93@gmail.com>
2022-01-13 13:34:02 +0100

    classwork

--- nine/ibrahim_ajao/ten_ten_game.py
+++ nine/ibrahim_ajao/ten_ten_game.py
@@ -1,5 +1,5 @@
-player_one_score = 0
-player_two_score = 0
+player_one_point = 0
+player_two_point = 0
 
 for i in range(5):
     play_one = input("Enter a value: ")
@@ -7,10 +7,10 @@ for i in range(5):
 
     if play_one == play_two:
         print("winner is leg two")
-        player_two_score += 1
+        player_two_point += 1
     elif play_one != play_two:
         print("Thw winner is leg one")
-        player_one_score += 1
+        player_one_point += 1
 
-print("Leg one score is ", player_one_score)
-print("Leg two score is ", player_two_score)
+print("Leg one score is ", player_one_point)
+print("Leg two score is ", player_two_point)
