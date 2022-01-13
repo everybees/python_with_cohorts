3fd2f082a95d9d689873b86fa8cf93a26d07d5d5
Ibrahim Ajao <ajaoibra93@gmail.com>
2022-01-13 13:44:11 +0100

    classwork2

--- nine/ibrahim_ajao/rock_paper_scissors.py
+++ nine/ibrahim_ajao/rock_paper_scissors.py
@@ -1,32 +1,33 @@
 
-user_one_score = 0
-user_two_score = 0
+player_one_score = 0
+player_two_score = 0
+
 for i in range(10):
-    user_one_input = input("Enter Rock Paper Or Scissors")
-    user_two_input = input("Enter Rock Paper Or Scissors")
+    player_one_pick = input("Enter Rock Paper Or Scissors")
+    player_two_pick = input("Enter Rock Paper Or Scissors")
 
-    if user_two_input == "rock" and user_one_input == "paper":
+    if player_two_pick == "rock" and player_one_pick == "paper":
         print("Player One Wins")
-        user_one_score += 1
+        player_one_score += 1
 
-    elif user_two_input == "paper" and user_one_input == "scissors":
+    elif player_two_pick == "paper" and player_one_pick == "scissors":
         print("Player One Wins")
-        user_one_score += 1
-    elif user_two_input == "scissors" and user_one_input == "rock":
+        player_one_score += 1
+    elif player_two_pick == "scissors" and player_one_pick == "rock":
         print("Player One Wins")
-        user_one_score += 1
-    elif user_two_input == user_one_input:
+        player_one_score += 1
+    elif player_two_pick == player_one_pick:
         print("Draw")
 
     else:
         print("Player Two Wins")
-        user_two_score += 1
-print("User One Score : ", user_one_score)
-print("User Two Score: ", user_two_score)
+        player_two_score += 1
+print("User One Score : ", player_one_score)
+print("User Two Score: ", player_two_score)
 
-if user_two_score > user_one_score:
+if player_two_score > player_one_score:
     print("User  Two Is The Winner !!!!")
-elif user_two_score == user_one_score:
+elif player_two_score == player_one_score:
     print("Stalemate")
 else:
     print("User One is the winner!!!!!")
