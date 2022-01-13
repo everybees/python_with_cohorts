4502d37e652e9d488a83a5df3fd3df659127b5a9
Joshua Sofowora <joshua.sofowora@gmail.com>
2022-01-13 05:09:04 +0100

    Class work

--- nine/joshua/ten_te.py
+++ nine/joshua/ten_te.py
@@ -7,16 +7,15 @@ twowins = 0
 while count < 10:
     count += 1
 
-    if (int(input('Type First Number: ')) == int(input('Type Second Number: '))) == True:
+    if int(input('Type First Number: ')) == int(input('Type Second Number: ')):
         print('Second player wins')
         twowins += 1
     else:
         print('First player wins')
         onewins += 1
 
-print('First player got: ',onewins, '\nSecond player got: ', twowins,)
+print('First player got: ', onewins, '\nSecond player got: ', twowins,)
 if onewins > twowins:
     print('Player One is the winner')
 else:
     print('Player Two is the winner')
-
