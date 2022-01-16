7950e2eaa3c46ab3078f231bb4007e8c198e45d7
Onwuka Lotachukwu <christiansolomon@gmail.com>
2022-01-16 23:30:29 +0100

    chapter two and three

---
+++ nine/lotachukwu/chapter_two/sort_in_ascending_order.py
@@ -0,0 +1,3 @@
+num1 = float(input('enter first number: '))
+num2 = float(input('enter second number: '))
+num3 = float(input('enter third number: '))
--- nine/lotachukwu/leap_year.py
+++ nine/lotachukwu/leap_year.py
@@ -1,5 +1,5 @@
 year = int(input('enter a year: '))
-#year1 = int(year)
+# year1 = int(year)
 if year % 4 == 0 and year % 100 != 0:
     print(year, "is a leap year")
 elif year % 100 == 0 and year % 400 == 0:
