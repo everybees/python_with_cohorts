08978f89b6d97af1928b86d058422dd45df48945
Adetunji Henry <teejaywurld@gmail.com>
2022-01-17 07:42:04 +0100

    Updated Code

---
+++ nine/adetunji_henry/chapter_two/multiples.py
@@ -0,0 +1,8 @@
+number1 = int(input("Enter your first number here: "))
+number2 = int(input("Enter your second number here: "))
+
+if (number1 % number2 == 0):
+    print("The first number you entered is a multiple of the second")
+else:
+    (number1 % number2 != 0)
+    print("The first number you entered is not a multiple of the second")
