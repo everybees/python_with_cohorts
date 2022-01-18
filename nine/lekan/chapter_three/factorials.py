ad9c0f3c0677bc8d6b5665f90a5dd4c523f4cd56
Abdulazeez Ibidapo <ibidapoazeez@gmail.com>
2022-01-18 11:51:41 +0100

    Updated Code

--- nine/lekan/chapter_three/factorials.py
+++ nine/lekan/chapter_three/factorials.py
@@ -1,7 +1,11 @@
 user_input = int(input('Enter A Number: '))
 product = 1
+
+if user_input <= 1:
+    product = 1
 for i in range(user_input):
     product *= user_input
     user_input -= 1
 
 print("The factorial is ", product)
+
