3d83a6b6b3ad22e39706191a8fef02419100fab3
Ibrahim Ajao <ajaoibra93@gmail.com>
2022-01-18 22:15:50 +0100

    classwork

---
+++ nine/ibrahim_ajao/class_work/list.py
@@ -0,0 +1,15 @@
+
+variable = [['Banana', 'Oranges', 'Mangoes'], ['Bread', 'Eggs', 'Tomatoes'], ['Rice', 'Beans', 'Yams']]
+for col in range(3):
+    for row in range(3):
+        print(col, row, end=' ', sep='-')
+        print(variable[col][row])
+
+
+#row = 2
+#while row >= 0:
+ #      col = 2
+#while col >= 0:
+ #          print(variable[col][row])
+  #         col -= 1
+   #        row -= 1
