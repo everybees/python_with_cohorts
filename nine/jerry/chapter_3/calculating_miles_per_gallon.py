ba12c0d46be25c9faf1798c4f6439d7cd63d83d6
Jerry Chibuife <chibuifejerryu2003@gmail.com>
2022-01-18 01:55:44 +0100

    assignment

--- nine/jerry/chapter_3/calculating_miles_per_gallon.py
+++ nine/jerry/chapter_3/calculating_miles_per_gallon.py
@@ -7,3 +7,16 @@
 # gallon obtained for all tankfuls (that is, total miles driven divided by total gallons used).
 
 
+total = 0
+miles = 0
+gallons_used = 0
+while miles != -1:
+    miles = float(input("Enter the number of miles driven: "))
+    gallons_used = float(input("Enter the number of diesel gallons used: "))
+    if miles == -1:
+        break
+    miles_per_gallon = gallons_used / miles
+    print(f'The miles per gallon for this tank was {miles_per_gallon:.2f}')
+    total += miles_per_gallon
+
+print(f'Total miles per gallon is {total:.2f}')
