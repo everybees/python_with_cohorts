03ccffee697f33c63b9369e16c34e53fe534d31d
Increase Uwadiale <increasebruce@gmail.com>
2022-01-18 12:23:24 +0100

    Updated_files

--- nine/increase/chapter_two/code.py
+++ nine/increase/chapter_two/code.py
@@ -12,3 +12,9 @@ print('x =', x)
 print('Value of', x, '+', x, 'is', (x + x))
 print('x =')
 print((x + y), '=', (y + x))
+
+# Answer
+# x = 2
+# Value of 2 + 2 is 4
+# x =
+# 5 = 5
---
+++ nine/increase/class_exercises_2/factorial.py
@@ -0,0 +1,9 @@
+number = int(input('Enter a number: '))
+factorial = 1
+
+if number == 0 or number == 1:
+     print(factorial)
+if number >= 2:
+    for i in range (number, 0,-1):
+      factorial *= i
+print(factorial)    
