4fdf1aab7072ac2061225403e72771546a1e1072
Sanni Oluwadamilola <sannijohnson5@gmail.com>
2022-01-19 15:26:19 +0100

    exercises

---
+++ nine/oluwadamilola/chapter_three.py/nested_loops.py
@@ -0,0 +1,18 @@
+for row in range (1, 11):
+    for column in range (0, row):
+        print('*', end=" ")
+    print()
+
+    for column in range (11, 1):
+                print('*', end= " ")
+print()
+
+for row in range (1, 11):
+    for column in range (11, row,-1):
+        print('*', end=" ")
+    print()
+
+for row in range (1,11):
+    for column in range(0):
+        print()
+    
---
+++ nine/oluwadamilola/chapter_three.py/turning_test.py
@@ -0,0 +1,6 @@
+prompt_1 = input ('What is your problem?')
+prompt_2 = input ('Have you had this problem before (yes or no)? ')
+if prompt_2 == 'yes':
+    print('Well, you have it again.')
+if prompt_2 == 'no':
+    print ('Well, you have it now.')
