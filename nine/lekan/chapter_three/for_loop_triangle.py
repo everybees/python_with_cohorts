070e9d785e9eccc75507f0d11d10c2b340d97c8a
Abdulazeez Ibidapo <ibidapoazeez@gmail.com>
2022-01-19 10:34:19 +0100

    Updated Code

--- nine/lekan/chapter_three/for_loop_triangle.py
+++ nine/lekan/chapter_three/for_loop_triangle.py
@@ -1,5 +1,5 @@
 
-for i in range(7):
+for i in range(1, 7):
     for j in range(1, i):
         print(j, end='')
 
--- nine/lekan/chapter_three/pythagorean_triples.py
+++ nine/lekan/chapter_three/pythagorean_triples.py
@@ -1,6 +1,6 @@
-for i in range(20):
-    for j in range(20):
-        for k in range(20):
+for i in range(21):
+    for j in range(21):
+        for k in range(21):
             pythagorean_triples = (i * i) + (j * j) == k * k
             if pythagorean_triples:
                 print(i, j, k)
