71f4ed43dbd6e61d4f21c27052df6c39955dd35d
Ibrahim Ajao <ajaoibra93@gmail.com>
2022-01-19 17:41:00 +0100

    updated

---
+++ nine/ibrahim_ajao/check.py
@@ -0,0 +1,20 @@
+name = "IbrahiM"
+#name_capitalize = name.capitalize()
+name_with_white_space = "   ajao"
+name_lstriped = name_with_white_space.lstrip("1")
+name_rstriped = name_with_white_space.rstrip("1")
+name_striped = name_with_white_space.strip("1")
+
+list_of_names =['Hellen', 'Tolu', 'Ibrahim', 'Motunrayo', 'Rogen' 'olamilekan', 'Jerry', 'Motun']
+for i in range(len(list_of_names)):
+   # print(list_of_names[i], len(list_of_names[i]))
+   if list_of_names[i].__contains__('gen'):
+    print("True")
+
+
+
+#name_lower = name.lower()
+
+#print(name_lower)
+#print(name_capitalize)
+#print(name_capitalize)
