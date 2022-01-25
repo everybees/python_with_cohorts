cfd75cec115d3219c4416f614cc2ccb56ba7ba2d
Mary Jane Okoroafor <maryjaneokoroafor77@gmail.com>
2022-01-25 11:09:18 +0100

    addedfiles

--- nine/maryjane/tuple_eggs.py
+++ nine/maryjane/tuple_eggs.py
@@ -3,3 +3,46 @@
 
 eggs=('medium', 'small' )
 print(eggs)
+
+#
+
+
+b='bread'
+e='eggs'
+f='fish'
+food_items=list(b)
+print(food_items)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# pleasantries= tuple('hello', )
+# print(pleasantries)
