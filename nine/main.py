7d9a7fddcb6f9f87e6cfe01ec7bc39fcac4f7954
everybees <everybees@gmail.com>
2022-01-26 09:57:45 +0100

    stuff

--- nine/main.py
+++ nine/main.py
@@ -1,22 +1,27 @@
-def main():
-    print("Welcome To Semicolon")
+# def main():
+#     print("Welcome To Semicolon")
 
 
-def leap_year(year):
-    leap = False
-    if year % 4 == 0 and (
-        year % 100 == 0 and year % 400 == 0 or year % 100 != 0
-    ):
-        leap=True
-    return leap
+# def leap_year(year):
+#     leap = False
+#     if year % 4 == 0 and (
+#         year % 100 == 0 and year % 400 == 0 or year % 100 != 0
+#     ):
+#         leap=True
+#     return leap
 
-    # a line solution for the same above code
-    # return (year % 4 == 0) and (year % 100 == 0 and year % 400 == 0 or year % 100 != 0)
+#     # a line solution for the same above code
+#     # return (year % 4 == 0) and (year % 100 == 0 and year % 400 == 0 or year % 100 != 0)
 
 
-if __name__ == "__main__":
-    # main()
+# if __name__ == "__main__":
+#     # main()
 
-    year = int(input("Enter year: "))
-    print(leap_year(year))
+#     year = int(input("Enter year: "))
+#     print(leap_year(year))
 
+
+# from bees.ten_ten import tired_class
+
+
+# tired_class()
