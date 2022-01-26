fb4176cae5aee252bed9d6a507d85630d3ee2825
Increase Uwadiale <increasebruce@gmail.com>
2022-01-26 12:17:22 +0100

    Updated my files

---
+++ nine/increase/chapter_two/python_integers.py
@@ -0,0 +1,9 @@
+# (How Big Can Python Integers Be?) We’ll answer this question later in the book.
+# For now, use the exponentiation operator ** with large and very large exponents to pro-
+# duce some huge integers and assign those to the variable number to see if Python accepts
+# them. Did you find any integer value that Python won’t accept?
+
+# prints
+print(768 ** 592)
+# doesn't print
+print(987876436774343435464 **1890000000000000000)
