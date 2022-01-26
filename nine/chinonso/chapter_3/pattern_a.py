5d446c216b7d4d80712233b3e23b6eea34080d69
chinonso Nwokonko <nwokonkochinonso@gmail.com>
2022-01-26 09:46:07 +0100

    ix gitignore

--- nine/chinonso/chapter_3/pattern_a.py
+++
@@ -1,5 +0,0 @@
-
-for a in range(1,7):
-    for b in range (1,a):
-        print(b, end=' ')
-    print()
--- nine/chinonso/chapter_3/pattern_b.py
+++
@@ -1,4 +0,0 @@
-for a in range(6,1,-1):
-    for b in range(1,a,+1):
-        print(b,end=' ')
-    print()
