f9eb3c469084a1e5756d7d53982495f455291179
Adeola Olarewaju <adeolaesther008@gmail.com>
2022-01-26 10:10:52 +0100

    push updates

--- nine/adeola_esther/chapter_four/loops.py
+++ nine/adeola_esther/chapter_four/loops.py
@@ -1,14 +1,18 @@
 
-number=0
 
-for row in range(1, 10, 2):
-    for column in range(1,row, +1):
-        print( '*', end=' ')
-    print('')
-
-
-for column in range(10,1 , 1):
-    for row in range(1,row, +1):
+for row in range(0, 11 ):
+    for column in range(0, row):
+        print( '*', end=" " )
+    print()
+print ()
+for row in range(0, 11):
+    for column in range(11, row, -1):
         print( '*', end=" ")
-    print('')
+    print()
+print()
+for row in range(11, 0):
+    for column in range(1, row ,11):
+        print( '*', end=" ")
+    print()
+print()
    
