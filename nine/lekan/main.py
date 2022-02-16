f0afcbf10bf5b7c4731c16cd60eb030615524c4b
Abdulazeez Ibidapo <ibidapoazeez@gmail.com>
2022-02-16 10:34:29 +0100

    Updated Code

--- nine/lekan/main.py
+++ nine/lekan/main.py
@@ -1,11 +1,6 @@
 
-
-from nine.lekan.ten_ten import ten_ten, tired_class
-
-tired_class()
-
 name = "Lekan Ibidapo"
 print("My name is", name)
-a_str = 'hello';
-a_str[1] = 'r';
-print(a_str)
+
+hidden = list(input())
+print(len(hidden))
